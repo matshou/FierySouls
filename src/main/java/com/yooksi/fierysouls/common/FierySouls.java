b1f6976a68ccc5dbabf65a4f629bd7151973b861
Matthew Cain <yooksi@zoho.com>
2016-09-27 08:10:32 +0200

    Fix custom EntityItem models
    Resolves: #9

--- src/main/java/com/yooksi/fierysouls/common/FierySouls.java
+++ src/main/java/com/yooksi/fierysouls/common/FierySouls.java
@@ -21,7 +21,7 @@ public class FierySouls
 {
 	public static final String MODID = "fierysouls";
 	public static final String NAME = "Fiery Souls";
-	public static final String VERSION = "1.4.7";
+	public static final String VERSION = "1.4.8";
 	
 	public static final String GUIFACTORY = "com.yooksi.fierysouls.common.FSGuiFactory";
 
